d4caa64208018fb8faa320e815b615e808e0f88a
nphjorth <nph@jfmedier.dk>
2023-12-01 14:12:18 +0100

    load cword data from file

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import ClueList from './ClueList';
 import Grid from './Grid';
+import jsonData from './data/data.json';
 
-const puzzle = 
-  {'rows': 12, 'cols': 12, 'answers': [{'col': 1, 'row': 1, 'down_or_across': 'down', 'clue': 'Realitet', 'answer': 'virkelighed', 'number': 1}, {'col': 3, 'row': 1, 'down_or_across': 'down', 'clue': 'Butik', 'answer': 'forretning', 'number': 2}, {'col': 1, 'row': 4, 'down_or_across': 'across', 'clue': 'Sangere', 'answer': 'kor', 'number': 3}, {'col': 5, 'row': 2, 'down_or_across': 'down', 'clue': 'Finansiere', 'answer': 'sponsorere', 'number': 4}, {'col': 6, 'row': 1, 'down_or_across': 'across', 'clue': 'Solid', 'answer': 'fast', 'number': 5}, {'col': 7, 'row': 1, 'down_or_across': 'down', 'clue': 'Kritik', 'answer': 'anmeldelse', 'number': 6}, {'col': 1, 'row': 9, 'down_or_across': 'across', 'clue': 'Købmænd', 'answer': 'handelsfolk', 'number': 7}, {'col': 7, 'row': 3, 'down_or_across': 'across', 'clue': 'Stil', 'answer': 'måde', 'number': 8}, {'col': 10, 'row': 2, 'down_or_across': 'down', 'clue': 'Start', 'answer': 'begyndelse', 'number': 9}, {'col': 10, 'row': 2, 'down_or_across': 'across', 'clue': 'Sted', 'answer': 'by', 'number': 9}, {'col': 9, 'row': 5, 'down_or_across': 'across', 'clue': 'Ø', 'answer': 'fyn', 'number': 10}, {'col': 4, 'row': 11, 'down_or_across': 'across', 'clue': 'Segment', 'answer': 'del', 'number': 11}, {'col': 8, 'row': 11, 'down_or_across': 'across', 'clue': 'Fremad', 'answer': 'frem', 'number': 12}]};
+const puzzleData = jsonData
+//console.log(puzzleData)
+const puzzle = puzzleData
 
   function App() {
     const [showSolutions, setShowSolutions] = useState(false);
